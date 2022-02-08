e8f4df7faf64dbf5d44475295e983de4a49e6c68
claudia <claudia_figueroai@hotmail.com>
2022-02-07 21:54:06 -0300

    se crea la funcion para el primer boton que tenga la clase display 4

---
+++ app.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+
+    let parrafo = $("#resultado p")
+    
+    $('.btn-primary').click(function() {
+        parrafo.addClass("display-4")
+    })
+
+
+
+
+
+
+})
+
